5dafb2a1afbe44aed5897e9d3ca8b5657e57cce4
Frederik Peter Høngaard <mail@frederikhoengaard.com>
2022-08-14 23:20:36 +0200

    Update experiments.py

--- src/autoforecast/experiments.py
+++ src/autoforecast/experiments.py
@@ -10,13 +10,13 @@ def predict_danish_power_consumption_2015(recursive=False):
     dk_daily = load_denmark(resample="daily")
     train, test = train_test_split_time(dk_daily, 365)
 
-    am = AutoModel()
+    am = AutoModel(verbose=2)
     print("fitting model")
     am.fit(dk_daily, "consumption_MW")
 
-    y_pred = am.recursive_forecast(365, verbose=4)
+    y_pred = am.recursive_forecast(365)
 
-    print(mean_absolute_percentage_error(test, y_pred))
+    print(mean_absolute_percentage_error(test, y_pred[1]))
 
 
 predict_danish_power_consumption_2015()
